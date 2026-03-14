f428fd381df6bbccba453ef909c763dc7f64f613
celvinandramaulana <celvin2103fv.2023@student.uny.ac.id>
2026-03-14 19:03:47 +0700

    fix umami hardcode

--- common/constants/umami.ts
+++ common/constants/umami.ts
@@ -1,23 +1,24 @@
 export const UMAMI_ACCOUNT = {
   username: "Celvin Andra Maulana",
-  api_key: process.env.UMAMI_API_KEY,
-  // Cukup sampai /v1 agar tidak bentrok di service
-  base_url: "https://api.umami.is/v1", 
+  // LANGSUNG PASTE KEY-NYA DI SINI
+  api_key: "api_0LZpZ8A2ITOJ6KWxEd3IFVvGxCZZfQTV", 
+  base_url: "https://api.umami.is/v1",
   endpoint: {
     page_views: "/pageviews",
     sessions: "/stats",
   },
   parameters: {
-    startAt: 1741885200000, // Tanggal hari ini dalam milidetik
-    endAt: 1767190799000, 
+    startAt: 1704067200000, // Dimundurin ke Jan 2024 biar data masuk
+    endAt: 1767190799000,
     unit: "month",
     timezone: "Asia/Jakarta",
   },
   is_active: true,
   websites: [
     {
-      domain: "celvinandra.my.id", 
-      website_id: process.env.UMAMI_WEBSITE_ID,
+      domain: "celvinandra.my.id",
+      // LANGSUNG PASTE ID-NYA DI SINI
+      website_id: "957f8e50-cea6-43bb-87b0-7b58328c4497",
       umami_url: "https://cloud.umami.is/websites/957f8e50-cea6-43bb-87b0-7b58328c4497",
     },
   ],
